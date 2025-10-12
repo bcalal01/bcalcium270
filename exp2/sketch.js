068f3b889a37947ae215f9cb4c5eaaeaa075697b
Brendan Calalang <166551282+bcalal01@users.noreply.github.com>
2025-10-12 02:10:47 -0400

    Update sketch.js

--- exp2/sketch.js
+++ exp2/sketch.js
@@ -48,7 +48,7 @@ class Dirt {
 
     render() {
         if (this.generate) {
-            ellipse(this.position.x, this.position.y, 3, 3);
+            ellipse(this.position.x, this.position.y, 100, 100);
             //text(cleaned, this.position.x, this.position.y);
         }
     }
